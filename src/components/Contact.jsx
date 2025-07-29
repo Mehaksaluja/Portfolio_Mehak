af3c233a30437426ef9fec639bbd5e70700d8dd8
Mehak Saluja <158473116+Mehaksaluja@users.noreply.github.com>
2025-07-29 21:54:50 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -49,7 +49,7 @@ const Contact = () => {
             </p>
             <div className="flex gap-4 mt-4">
               <a
-                href="" //https://www.linkedin.com/in/mehak-saluja/"
+                href="#" //https://www.linkedin.com/in/mehak-saluja/"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-[#F9A826] hover:text-white transition"
@@ -57,7 +57,7 @@ const Contact = () => {
                 <FaLinkedin size={24} />
               </a>
               <a
-                href="" //https://github.com/Mehaksaluja
+                href="#" //https://github.com/Mehaksaluja
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-[#F9A826] hover:text-white transition"
@@ -65,7 +65,7 @@ const Contact = () => {
                 <FaGithub size={24} />
               </a>
               <a
-                href="" //https://x.com/salujamehak5
+                href="#" //https://x.com/salujamehak5
                 target="_blank" 
                 rel="noopener noreferrer"
                 className="text-[#F9A826] hover:text-white transition"
